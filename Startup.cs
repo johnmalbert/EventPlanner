6526bb779d96de784951a85caf91e7acbf0b6ece
John Albert <john.m.albert@gmail.com>
2021-03-01 13:49:27 -0800

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
-using Event_Planner.Models;
+using EventPlanner.Models;
 
 namespace EventPlanner
 {
