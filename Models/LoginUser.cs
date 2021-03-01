81949653cb8487bb19029af213d7051363523a28
John Albert <john.m.albert@gmail.com>
2021-03-01 15:22:47 -0800

    Create LoginUser.cs

---
+++ Models/LoginUser.cs
@@ -0,0 +1,22 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Scema;
+
+namespace EventPlanner.Models
+{
+    public class LoginUser
+    {
+        [Key]
+        public int LoginUserId { get; set; }
+
+
+        [Required(ErrorMessage="Please provide your email.")]
+        [EmailAddress(ErrorMessage="Please provide a valid email.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage="Please provide your password.")]
+        [DataType(DataType.Password)]
+        public string LoginPassword { get; set; }
+
+    }
+}
