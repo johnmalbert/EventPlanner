b2354b5451cb672129bd53787ffff88a7259778e
QLathe <quinn.s.lathe@gmail.com>
2021-03-01 18:59:22 -0800

    Update HomeController.cs
    Added the two HttpPost requests. First for registration, second for login. Saves user Id is session under Logged User

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -39,6 +39,52 @@ namespace EventPlanner.Controllers
         {
             return View();
         }
+
+        [HttpPost("registration")]
+        public IActionResult Registration(User user){
+            if(ModelState.IsValid)
+            {
+                if (_context.Users.Any(u => u.Email == user.Email))
+                {
+                    ModelState.AddModelError("Email", "Email is already in user");
+                    return View("Register");
+                }
+
+                PasswordHasher<User> Hasher = new PasswordHasher<User>();
+                user.Password = Hasher.HashPassword(user, user.Password);
+                _context.Add(user);
+                _context.SaveChanges();
+                HttpContext.Session.SetInt32("LoggedUser", user.UserId);
+                return RedirectToAction("Dashboard");
+            }
+            return View("Register");
+        }
+
+        [HttpPost("logging")]
+        public IActionResult Logging(LoginUser userSubmission)
+        {
+            if (ModelState.IsValid)
+            {
+                var userInDb = _context.Users.FirstOrDefault(u => u.Email == userSubmission.Email);
+                if (userInDb == null)
+                {
+                    ModelState.AddModelError("Email", "Invalide Email/Password");
+                    return View("Login");
+
+                }
+                var hasher = new PasswordHasher<LoginUser>();
+                var result = hasher.VerifyHashedPassword(userSubmission, userInDb.Password, userSubmission.LoginPassword);
+                if (result == 0)
+                {
+                    ModelState.AddModelError("Email", "Invalide Email/Password");
+                    return View("Login");
+                }
+                HttpContext.Session.SetInt32("LoggedUser", userInDb.UserId);
+                return RedirectToAction("Dashboard");
+            }
+            return View("Login");
+        }
+
         [HttpGet("Dashboard")]
         public IActionResult Dashboard()
         {
