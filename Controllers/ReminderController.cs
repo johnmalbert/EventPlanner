c0427c9d779fd8cd63fb66e5078a3406d284b4e6
John Albert <john.m.albert@gmail.com>
2021-03-03 11:38:46 -0800

    Added Reminders to DB upon creation
    Gave option to delete existing reminders.

--- Controllers/ReminderController.cs
+++ Controllers/ReminderController.cs
@@ -27,6 +27,8 @@ namespace EventPlanner.Controllers
         {
             //get the current user
             User CurrentUser = LoggedUser();
+            if(CurrentUser == null)
+                return RedirectToAction("Index");
             
             //get all the events for the current user
             ViewBag.Events = _context.Events.Where(e => e.Creator == CurrentUser);
@@ -39,13 +41,32 @@ namespace EventPlanner.Controllers
         {
             //get the current user
             User CurrentUser = LoggedUser();
+            if(CurrentUser == null)
+                return RedirectToAction("Index");
+
             //get the event 
             Event CurrentEvent = _context.Events.FirstOrDefault(e => e.EventId == num);
 
             ViewBag.CurrentUser = CurrentUser;
             ViewBag.CurrentEvent = CurrentEvent;
+            ViewBag.Reminders = _context.Reminders
+                .Include(r => r.User)
+                .Include(r => r.Event)
+                .Where(r => r.User == CurrentUser && r.Event == CurrentEvent);
             //return view
             return View();
+        }
+
+        [HttpGet("reminder/{reminderId}/delete")]
+        public IActionResult DeleteReminder(int reminderId)
+        {
+            //get the reminder by number
+            Reminder reminderToDelete = _context.Reminders.FirstOrDefault(r => r.ReminderId == reminderId);
+            int EventId = reminderToDelete.EventId;
+            _context.Remove(reminderToDelete);
+            _context.SaveChanges();
+            Console.WriteLine("Deleted a Reminder.");
+            return Redirect($"/reminder/{EventId}");
         }
 
         public User LoggedUser() // this will return null if the user isn't logged in.
@@ -79,6 +100,9 @@ namespace EventPlanner.Controllers
                 to = CurrentUser.Email,
                 TimeToSendReminder = setReminder
             };
+            _context.Add(newReminder);
+            _context.SaveChanges();
+
             Console.WriteLine($"Reminder will be sent to {newReminder.to}, set for {newReminder.TimeToSendReminder}");
 
             return Redirect($"/reminder/{CurrentEvent.EventId}");
