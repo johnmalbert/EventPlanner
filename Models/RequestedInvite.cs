206be081d2530a0ca15203fef5c28849fac603a7
John Albert <john.m.albert@gmail.com>
2021-03-04 10:50:36 -0800

    Created Invite and requestedInvite
    Unable to add tables to DB...
    You can now invite users on the DisplayEvent Page

---
+++ Models/RequestedInvite.cs
@@ -0,0 +1,16 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace EventPlanner.Models
+{
+    public class RequestedInvite
+    {
+        [Key]
+        public int RequestedInviteId { get; set; }
+        public int UserId { get; set; } // this is the target
+        public User User { get; set; }
+        public int Requester { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
+    }
+}
